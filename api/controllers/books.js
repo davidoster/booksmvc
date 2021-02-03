3d2b3d828e9dcf9083c4c4d25b4c839e97e17c27
David Oster <paspa@hotmail.com>
2021-02-03 20:35:40 +0200

    db connection and show array of book on view

--- api/controllers/books.js
+++ api/controllers/books.js
@@ -1,3 +1,37 @@
+var mysql2 = require("mysql2");
+
+var connection = mysql2.createConnection({
+    host:'ra1.anystream.eu',
+    user:'cb12ptjs',
+    password:'cb12ptjs',
+    database:'cb12ptjs',
+    port:5420
+});
+
+connection.connect(function(error) {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log("Connected!");
+    }
+});
+
+var books;
+const query = "SELECT * FROM books";
+connection.query(query, function(err, rows) {
+    books = rows;
+});
+
+var myBooksClassObject = class Books {
+    constructor() {
+        this.id =  '11111';
+        this.title = 'aaaaaa';
+    }
+}
+
+var xxx = new myBooksClassObject();
+
+
 module.exports = {
     exits: {
         success: { 
@@ -6,6 +40,6 @@ module.exports = {
     },
 
     fn: async function() {
-        return {'id': 1, 'title': "AAAAA"};
+        return {books, xxx, zzz: 10}
     }
 }
